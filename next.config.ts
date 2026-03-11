0dc3bf41b9581c09187201858517954c7dc09700
faraz aleem <farazaleem@gmail.com>
2026-03-11 14:29:56 -0700

    added redirect only

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-
+  async redirects() {
+    return [
+      {
+        source: '/',                       // Homepage
+        destination: '/eventlist-wireframe', // Target page
+        permanent: true,                    // 301 redirect
+      },
+    ];
+  },
 };
 
 export default nextConfig;
