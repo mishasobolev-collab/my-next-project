94c5f2f25e572128933109626d1421ac843ef5ee
faraz aleem <farazaleem@gmail.com>
2026-03-15 08:19:01 -0700

    Updated content arr in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
+  content: [
+  "./src/**/*.{js,ts,jsx,tsx,mdx}",
+  "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+  "./components/**/*.{js,ts,jsx,tsx,mdx}",
+  "./lib/**/*.{js,ts,jsx,tsx,mdx}",
+],
   theme: {
     extend: {
       fontFamily: {
